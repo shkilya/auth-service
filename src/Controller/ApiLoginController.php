bebb14a3eaf9870ec868bb609f72a25266c095f8
shkilniuky <yevhenii.shkilniuk@btcbit.net>
2021-10-21 16:26:11 +0300

    user reposity

---
+++ src/Controller/ApiLoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ApiLoginController extends AbstractController
+{
+    public function __construct()
+    {
+    }
+
+    #[Route('/api/login', name: 'api_login')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ApiLoginController.php',
+        ]);
+    }
+}
